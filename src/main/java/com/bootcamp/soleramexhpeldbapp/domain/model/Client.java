7e21b57349a7c9324050f2c0aef2fba1fbe40ca8
Svetlana.Shemiakina <scenacheros@yandex.ru>
2022-11-17 09:47:21 +0100

    Initial commit

---
+++ src/main/java/com/bootcamp/soleramexhpeldbapp/domain/model/Client.java
@@ -0,0 +1,2 @@
+package com.bootcamp.soleramexhpeldbapp.domain.model;public class Client {
+}
---
+++ src/test/java/com/bootcamp/soleramexhpeldbapp/SoleramexhpelDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.soleramexhpeldbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleramexhpelDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
