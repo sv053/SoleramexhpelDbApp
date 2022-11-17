f6c44b179793bb30d75d08c9220d017990e19362
Svetlana.Shemiakina <scenacheros@yandex.ru>
2022-11-17 09:51:19 +0100

    initial commit

--- src/main/java/com/bootcamp/soleramexhpeldbapp/domain/model/Client.java
+++ src/main/java/com/bootcamp/soleramexhpeldbapp/domain/model/Client.java
@@ -1,2 +1,8 @@
-package com.bootcamp.soleramexhpeldbapp.domain.model;public class Client {
+package com.bootcamp.soleramexhpeldbapp.domain.model;
+
+public class Client {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
 }
